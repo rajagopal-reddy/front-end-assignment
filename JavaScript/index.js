883f31941bdd8daa01fc3d66578b727b0ed5bd40
rajagopal-reddy <rajagopalreddy337@gmail.com>
2024-11-28 09:45:34 +0530

    chore: loops condition

---
+++ JavaScript/index.js
@@ -0,0 +1,21 @@
+let a = 10;
+let b = 8;
+console.log(a>b?"Num1 is greater":"Num2 is greater");
+
+var i = 1;
+while(i<=5){
+  console.log("While loop iteration "+i);
+  i++;
+}
+
+for(let i=1;i<=5;i++){
+  console.log("For loop iteration "+i);
+}
+var i = 1;
+do{
+  console.log("Do while loop iteration "+i);
+  i++;
+}while(i<=5);
+
+var week = "Monday";
+console.log(week == "Monday" ? "It's Monday" : "It's not Monday");
